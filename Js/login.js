69e34d0214d24235a2f6364ccaf930bc37d29137
Safiaahmed6043 <safiaahmed1786@gmail.com>
2021-09-16 14:18:19 +0600

    login js complete

---
+++ Js/login.js
@@ -0,0 +1,17 @@
+const emailInput= document.getElementById("emailInput");
+const passWordInput= document.getElementById("passWordInput");
+const loginButton= document.getElementById("Login");
+
+passWordInput.addEventListener("keydown",function(event){
+    if(event.key ==="enter"){
+        loginButton.click();
+    }
+})
+
+loginButton.addEventListener("click",()=>{
+    const emailInputValue=emailInput.value;
+    const PassWordInputValue=passWordInput.value;
+    if(emailInputValue=="safia@gmail.com" && PassWordInputValue=="1234"){
+        window.location.href="home.html";
+    }
+})
